51b2cd84cfde64b443d9847f84c2c0577506a2aa
Kirtan Tank <tankkirtan5@gmail.com>
2023-08-06 20:15:46 +0530

    added the post and get requests

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,45 +1,52 @@
 import {useState} from 'react';
+import axios from 'axios';
 
 const Register = () => {
-
-    const [name, setName] = useState("");
-    const [address, setAddress] = useState("");
-    const [contact, setContact] = useState("");
-
+    const [newRestaurant, setNewRestaurant] = useState({
+        name: '',
+        address: '',
+        contact: ''
+    }); 
+    
+    const handleInput = (event) => {
+        setNewRestaurant({...newRestaurant, [event.target.name]: event.target.value})
+    }
     const handleSubmit = async (e) => {
         e.preventDefault();
-        const RestaurantPayload = {
-            address,
-            contact,
-            name
-        }
-        // console.log(VendorPayload);
+
         // Send Data to Server
-       
-        setAddress("");
-        setContact("");
-        setName("");
+        // console.log(newRestaurant);
+        axios.post('http://localhost:5000/addRestaurant', newRestaurant)
+        .then(res => {
+            // console.log(res);
+            setNewRestaurant({
+                name: '',
+                address: '',
+                contact: ''
+            });
+        })
+        .catch(err => console.log(err));
     }
     return (
         <div className="max-w-2xl bg-white px-5 m-auto w-full">
             <div className="text-3xl mb-6 text-center ">Add Restaurant</div>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className="grid grid-cols-2 gap-4 max-w-xl m-auto">
 
                     <div className="col-span-2">
-                        <input type="text" value={name} onChange={({ target }) => setName(target.value)} className="border-solid border-gray-400 border-2 p-3 md:text-xl w-full rounded-md" id="vendorName" placeholder="Restaurant Name" required />
+                        <input type="text" name='name' onChange={handleInput} className="border-solid border-gray-400 border-2 p-3 md:text-xl w-full rounded-md" id="restaurantName" placeholder="Restaurant Name" required />
                     </div>
 
                     <div className="col-span-2">
-                        <input type="text" value={address} onChange={({ target }) => setAddress(target.value)} className="border-solid border-gray-400 border-2 p-3 md:text-xl w-full rounded-md" id="bankName" placeholder="Address" required />
+                        <input type="text" name='address' onChange={handleInput} className="border-solid border-gray-400 border-2 p-3 md:text-xl w-full rounded-md" id="restaurantAddress" placeholder="Address" required />
                     </div>
 
                     <div className="col-span-2">
-                        <input type="number" value={contact} onChange={({ target }) => setContact(target.value)} cols="30" rows="8" className="border-solid border-gray-400 border-2 p-3 md:text-xl w-full rounded-md" id="accNumber" placeholder="Contact Number" required />
+                        <input type="number" name='contact' onChange={handleInput} cols="30" rows="8" className="border-solid border-gray-400 border-2 p-3 md:text-xl w-full rounded-md" id="restaurantContact" placeholder="Contact Number" required />
                     </div>
 
                     <div className="col-span-2 text-right">
-                        <button className="py-3 px-6  rounded-md bg-green-500 text-white font-bold w-full sm:w-32" type="submit" onClick={handleSubmit}>CREATE</button>
+                        <button className="py-3 px-6  rounded-md bg-green-500 text-white font-bold w-full sm:w-32">CREATE</button>
                     </div>
                 </div>
             </form>
